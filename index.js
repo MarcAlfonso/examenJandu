a5b575a59ff72527718736874ee97638d49e1239
ALFONSO_SECANELLA <marc.alfonso@balmes.escolapia.net>
2018-02-19 17:44:26 +0100

    intento fallido de hacer los ejercicio 2 y 3

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hola");
+var express = require("express");
+var bodyParser = require('body-parser'); 
+var app = express();
+var fileType = require("file-type");
+const http = require("http");
+const url = "https://www.jandusoft.com/images/JanduSoft_logo.png";
+
+app.use(bodyParser.json());
+
+var words = { palabras: "hola,como,estas,hola,como,estas,hola,como,estas"};
+var str = "hola llamando a papa";
+
+app.get('/removeduplicatewords', function(req, res){
+		var hola = str.split("a");
+		res.send(hola);
+});
+
+app.get('/detectfiletype', function(req, res){
+		res.send("file type working");
+});
+http.get(url, res){
+    res.once('data', chunk => {
+        res.destroy();
+        console.log(fileType(chunk));
+        //=> {ext: 'gif', mime: 'image/gif'}
+    });
+
+
+app.listen(8000);
